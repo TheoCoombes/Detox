4aa3674b1adfaeaa4d31b40144f7d5ce47ebd88d
Theo Coombes <theocoombes06@gmail.com>
2026-01-18 11:57:40 +0000

    Rename function to remove Reels navigation links

--- detox-instagram.user.js
+++ detox-instagram.user.js
@@ -164,7 +164,7 @@
     function runAll() {
         forceRedirectIfReels();
         handleMainVisibility();
-        removeExploreAndReelsNavLinks();
+        removeReelsNavLinks();
         removeAdsAndSponsoredPosts();
     }
 
